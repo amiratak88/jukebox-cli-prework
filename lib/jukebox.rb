d83ba8f38827d2edd2d851b3473bdba740048074
Amirata K <amiratak88@gmail.com>
2018-07-09 15:38:13 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -39,7 +39,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   answer = gets.chomp
   if answer.class == Integer
-    (1..songs.length).include?(answer) ? puts "Playing #{songs [answer - 1]}" : puts "Invalid input, please try again"
+    ((1..songs.length).include?(answer)) ? puts "Playing #{songs [answer - 1]}" : puts "Invalid input, please try again"
   else
     songs.include?(answer) ? puts "Playing #{answer}" : puts "Invalid input, please try again"
   end
